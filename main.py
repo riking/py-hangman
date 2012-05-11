6e515c6c8097260c9156dfd25614f0df52de140c
Riking <rikingcoding@gmail.com>
2012-05-11 11:07:01 -0700

    Fully functional

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # stdlib
-import getpass
+import getpass, re
+import sys, time
+
 # libraries
 import getch
+
 # program
 import word
 
@@ -27,33 +30,86 @@ def getDrawing(strikes):
      __|__"""
 
 
+
 class HangmanGame:
     def __init__(self):
         self.usedletters = [False for i in range(26)]
         self.word = None
         self.strikes = 0
 
+
     def play(self):
         print('\n')
         print("Please pass the computer to the word-maker.")
+        time.sleep(0.5)
         w = getpass.getpass("Word: ")
         self.word = word.Word(w)
         print("Pass the computer to the players")
+        time.sleep(1)
         while(1):
+            # Print the gameboard
             print("\n   %s/8   "%self.strikes+''.join(['-' if self.usedletters[i] else chr(i+65) for i in range(26)]))
             lines=getDrawing(self.strikes).split('\n')
             lines[3] += '    '+str(self.word)
             print('\n'.join(lines))
-            a=""
+
+            time.sleep(0.2)
+            # Game over checks
+            if self.word.isWordGuessed():
+                return True
+            elif self.strikes == 8:
+                return False
+
+            time.sleep(0.5)
+            # input
+            a=None
             while(1):
-                a=getch.getch("Choose a letter, or * to guess: ")
-                if len(a) != 1:
-                    print("Only one letter, please")
+                sys.stdout.write("Choose a letter, * to guess, or & to quit: ")
+                a=getch.getch().upper()
+                if re.match(r'[\w]',a):
+                    if self.usedletters[ord(a)-65]:
+                        print("You already used that!")
+                        continue
+                    else:
+                        sys.stdout.write(a) # echo the letter
+                        break
+                elif a=='&': # Quit
+                    return None
+                elif a=='*':
+                    self.guessWord()
+                    a=None
+                elif a==chr(3):
+                    raise KeyboardInterrupt
+                print("Not a valid letter")
+            if a!=None:
+                ind = ord(a)-65
+                self.usedletters[ind]=True
+                result = self.word.guess(a)
+                if(result):
+                    print("\nYou got one!")
+                else:
+                    print("\nNope!")
+                    self.strikes += 1
+            # gameloop
+        #end of play()
+
+
+    def guessWord():
+        pass
+
+
+    def playAgain(self,result):
+        if result != None:
+            print("Game over! Looks like you "+('won' if result else 'lost')+"!")
+        i = raw_input("Want to play again? (Y/n) ")
+        # first letter of 'no' and 'quit'
+        if i[0] in 'NnQq':
+            return False
+        return True
 
 if __name__=="__main__":
     g=HangmanGame()
-    g.play()
-    while(g.playAgain()):
-        g.play()
+    while(g.playAgain(g.play())):
+        pass
     raise SystemExit
 
--- word.py
+++ word.py
@@ -2,20 +2,43 @@
 
 class Word:
     def __init__(self, answer):
-        self.answer = answer
+        self.answer = answer.upper()
+        # array of bools, represent if each letter is visible
         self.guessed = [False for i in range(len(answer))]
+        self.gameOver = False
+
 
     # @return: true if letter was found
     def guess(self,letter):
         ret = False
+        l = letter.upper()
         for i in range(len(self.answer)):
             if self.answer[i] == letter:
                 ret = True
                 self.guessed[i] = True
         return ret
 
+
+    def isWordGuessed(self):
+        if self.gameOver:
+            return True
+        for i in range(len(self.answer)):
+            if not self.guessed[i]:
+                return False
+        self.gameOver = True
+        return True
+
+
+    def longGuess(self,g):
+        r = g.upper() == self.answer
+        if r:
+            self.gameover = True
+        return r
+
+
     def __len__(self):
         return len(self.answer)
+
 
     def __str__(self):
         return ' '.join([ (self.answer[i] if self.guessed[i] else '_') for i in range(len(self.answer)) ])
